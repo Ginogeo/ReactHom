6177bd883af2d3947b4c2e7918fc0badeff0e63a
Gino_Geo <ginogeo351@gmail.com>
2025-03-22 09:57:45 +0530

    Add Footer component and integrate it into the App

--- Rent-a-car/src/App.jsx
+++ Rent-a-car/src/App.jsx
@@ -5,6 +5,7 @@ import HowItWorks from './HowItWorks.jsx'
 import Features from './Features.jsx'
 import RentalDeals from './RentalDeals.jsx'
 import TestimonialsSection from './TestimonialsSection.jsx'
+import Footer from './Footer.jsx'
 function App() {
   
 
@@ -17,6 +18,7 @@ function App() {
           <Features/>
           <RentalDeals/>
           <TestimonialsSection/>
+          <Footer/>
       </div>
         
     </>
